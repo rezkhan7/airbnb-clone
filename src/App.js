589dd37b9fb2069ad4fa6b646f91f9e53bf77777
Rezaur Khan <rezaurkhan@Rezaurs-MacBook-Air.local>
2022-01-04 21:07:48 -0500

    Added the Hero Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Navbar from "./components/Navbar.js"
+import Hero from "./components/Hero.js"
 import * as style from "./style.css"
 
 let App = ()=>{
     return(
        <div>
            <Navbar />
+           <Hero/>
        </div>
     )
 }
